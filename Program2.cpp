dc84c88ca75fd9fe9675e77968857a9f59a59cd7
Tarushi Verma <tarushiv2611@gmail.com>
2025-10-04 21:31:14 +0530

    Program2.cpp

---
+++ Program2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main(){
+    float n1,n2,ans;
+    std:: cout<<"enter value of number 1 & 2: ";
+    std::cin>>n1>>n2;
+    try{
+        if(n2==0){
+            throw n2;
+        }
+        else{
+            ans=n1/n2;
+            std::cout<<"answer = " <<ans<<"\n";
+        }
+    }
+    catch(float num){
+        std::cout << "\nERROR : division by " << num << "\n";
+    }
+}
+/*output:
+enter value of number 1 & 2: 34
+45
+answer = 0.755556
+*/
